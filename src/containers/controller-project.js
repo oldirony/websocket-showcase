132486d96f78f0b37f8bcd6c0478e60c900c3dd9
raffaeleabramini <raffaele.abramini@gmail.com>
2017-03-06 11:46:14 +0000

    Controller style

--- src/containers/controller-project.js
+++ src/containers/controller-project.js
@@ -19,9 +19,20 @@ class ControllerProject extends Component {
 	render() {
 		if(!this.props.currentProject){ return <div></div>; }
 
-		return <div className="c-controller-project">
-			<h1>{this.props.currentProject.title}</h1>
-			<button className="c-button-icon" onClick={this.handleCloseClick.bind(this)}><svg className="o-icon"><use xlinkHref="#icon-close"/></svg></button>
+		return <div className="c-controller-project o-controller-project">
+			<div className="o-controller-project__main-content">
+				<header className="c-controller-project__header">
+					<h1>{this.props.currentProject.title}</h1>
+				</header>
+				<div className="c-controller-project__views">
+					{this.props.children}
+				</div>
+			</div>
+			<nav className="o-controller-side-nav o-controller-project__side-buttons">
+				<button className="c-button-icon o-controller-side-nav__button" onClick={this.handleCloseClick.bind(this)}><svg className="o-icon"><use xlinkHref="#icon-close"/></svg></button>
+				<button className="c-button-icon o-controller-side-nav__button" onClick={this.handleCloseClick.bind(this)}><svg className="o-icon"><use xlinkHref="#icon-people"/></svg></button>
+				<button className="c-button-icon o-controller-side-nav__button" onClick={this.handleCloseClick.bind(this)}><svg className="o-icon"><use xlinkHref="#icon-timeline"/></svg></button>
+			</nav>
 		</div>
 	}
 
